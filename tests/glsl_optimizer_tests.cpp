bb1d06a2bb354abd08c6bb9358a822915b5e97e7
Urs Hanselmann <git@hanselmann.io>
2025-02-17 17:21:36 +0100

    refactor unit tests

--- tests/glsl_optimizer_tests.cpp
+++ tests/glsl_optimizer_tests.cpp
@@ -2,19 +2,35 @@
 #include "gmock/gmock.h"
 
 #include <iostream>
+#include <utility>
+#include <stdexcept>
 
 #include "glsl_optimizer.h"
+
+#define VERTEX_SHADER kGlslOptShaderVertex
+#define FRAGMENT_SHADER kGlslOptShaderFragment
+
+#define TEST_COMPILE_SHADER(type, src, expected) \
+    auto [success, output] = compileShader(type, src); \
+    EXPECT_TRUE(success) << "failed to compile shader: " << output; \
+    EXPECT_EQ(output, expected)
 
 using namespace ::testing;
 
 namespace {
 
-// NOLINTNEXTLINE
-TEST(GlslOptimTests, CanOptimizeSingleShader)
+class OptimizerTest : public ::testing::Test
 {
-    auto ctx = glslopt_initialize(kGlslTargetOpenGLES20);
+public:
+    glslopt_target shaderTargetLang { kGlslTargetOpenGLES20 };
 
-    const char* vShaderSrc = R""""(
+    std::pair<bool, std::string> compileShader(glslopt_shader_type type, std::string shaderSrc);
+};
+
+// NOLINTNEXTLINE
+TEST_F(OptimizerTest, VertexShader)
+{
+    TEST_COMPILE_SHADER(VERTEX_SHADER, R""""(
 attribute vec4 vPosition;
 attribute vec4 vColor;
 attribute vec2 vTexcoord;
@@ -27,13 +43,7 @@ void main() {
     color = vColor;
     uv = vTexcoord;
 }
-    )"""";
-
-    auto shader = glslopt_optimize (ctx, kGlslOptShaderVertex, vShaderSrc, 0);
-    EXPECT_TRUE(glslopt_get_status (shader)) << "failed to compile shader: " << glslopt_get_log (shader);
-
-    std::string output = glslopt_get_output(shader);
-    EXPECT_EQ(output, R""""(attribute highp vec4 vPosition;
+    )"""", R""""(attribute highp vec4 vPosition;
 attribute highp vec4 vColor;
 attribute highp vec2 vTexcoord;
 varying highp vec4 color;
@@ -43,11 +53,28 @@ void main ()
   gl_Position = vPosition;
   color = vColor;
   uv = vTexcoord;
+})"""");
 }
 
-)"""");
+std::pair<bool, std::string> OptimizerTest::compileShader(glslopt_shader_type type, std::string shaderSrc)
+{
+    auto ctx = glslopt_initialize(shaderTargetLang);
+    auto shader = glslopt_optimize (ctx, type, shaderSrc.c_str(), 0);
+    bool success = glslopt_get_status (shader);
+
+    const char* outp = success ? glslopt_get_output(shader) : glslopt_get_log(shader);
+    std::string output = outp ? std::string(outp) : std::string{};
 
     glslopt_cleanup(ctx);
+
+    if (!outp)
+        throw std::runtime_error { "unexpected null pointer" };
+
+    while (!output.empty() && output.back() == '\n') {
+        output.pop_back();
+    }
+
+    return std::make_pair(success, output);
 }
 
 }
